46aecc0abca500b6e2088003dc36a4734b32f3aa
Nomik94 <kill0374@naver.com>
2023-03-01 13:22:52 +0900

    Feat : 회원가입 기능

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/database/entities/user.entity';
 import { Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credential.dto';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
@@ -9,4 +15,25 @@ export class AuthService {
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
   ) {}
+
+  async register(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { email, username, password } = authCredentialsDto;
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = this.userRepository.create({
+      email,
+      username,
+      password: hashedPassword,
+    });
+    try {
+      await this.userRepository.save(user);
+    } catch (e) {
+      if (e.code === '23505') {
+        throw new ConflictException('이미 있는 이메일입니다');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
 }
