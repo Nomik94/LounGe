038e193212c047f2420106fbd9b8cdae2376ea34
Nomik94 <kill0374@naver.com>
2023-03-02 09:52:16 +0900

    Rename : 회원가입 기능 auth에서 register로 폴더이름 변경

---
+++ src/register/register.controller.ts
@@ -0,0 +1,15 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { RegisterService } from './register.service';
+import { RegisterCredentialsDto } from './dto/register-credential.dto';
+
+@Controller('api/register')
+export class RegisterController {
+  constructor(private readonly registerService: RegisterService) {}
+
+  @Post('register')
+  register(
+    @Body() registerCredentialsDto: RegisterCredentialsDto,
+  ): Promise<void> {
+    return this.registerService.register(registerCredentialsDto);
+  }
+}
