0ba76cc1a3e9e58f94e94c4757eb02bae8c25e0a
OHGILHWAN <dhrlfghks01@gmail.com>
2023-03-03 03:01:34 +0900

    Feat : 그룹 수정 기능 추가 #17

--- src/group/dto/modify.group.dto.ts
+++ src/group/dto/modify.group.dto.ts
@@ -1,17 +1,21 @@
 import { IsString, MaxLength, MinLength, IsOptional, IsArray } from 'class-validator';
 
-export class CreateGroupDto {
+export class ModifyGroupDto {
+  @IsOptional()
   @IsString()
   @MaxLength(10)
   @MinLength(1)
   groupName: string;
 
+  @IsOptional()
   @IsString()
   description: string;
 
+  @IsOptional()
   @IsString()
   groupImage: string;
 
+  @IsOptional()
   @IsString()
   backgroundImage: string;
 
--- src/group/group.controller.ts
+++ src/group/group.controller.ts
@@ -1,7 +1,9 @@
 import { Body, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { Controller } from '@nestjs/common';
+import { Param, Put } from '@nestjs/common/decorators';
 import { AuthGuard } from '@nestjs/passport';
 import { CreateGroupDto } from './dto/create.group.dto';
+import { ModifyGroupDto } from './dto/modify.group.dto';
 import { GroupService } from './group.service';
 
 @Controller('/api/groups')
@@ -11,14 +13,23 @@ export class GroupController {
   @Post()
   @UseGuards(AuthGuard('jwt'))
   createGroup(@Req() req, @Body() data: CreateGroupDto): void {
-    const userId : number = req.user.id
+    const userId: number = req.user.id;
     this.groupService.createGroup(data, userId);
   }
 
   @Get()
   @UseGuards(AuthGuard('jwt'))
   async getAllGroup(@Req() req) {
-    const userId : number = req.user.id;
+    const userId: number = req.user.id;
     return await this.groupService.getAllGroup(userId);
+  }
+
+  @Put('/:groupId')
+  @UseGuards(AuthGuard('jwt'))
+  async modifyGruop(@Req() req, @Body() data: ModifyGroupDto, @Param('groupId') groupId : number) {
+    const userId: number = req.user.id;
+    console.log(typeof(groupId))
+    console.log(data)
+    this.groupService.modifyGruop(data, userId, groupId)
   }
 }
