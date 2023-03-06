526d8fa6ffa330ae7c846bec3fc24e0964c73d68
OHGILHWAN <dhrlfghks01@gmail.com>
2023-03-06 22:47:58 +0900

    Refactor : 이미지 업로드 추가

--- src/group/group.module.ts
+++ src/group/group.module.ts
@@ -1,14 +1,21 @@
 import { Module } from '@nestjs/common';
+import { MulterModule } from '@nestjs/platform-express';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { NestjsFormDataModule } from 'nestjs-form-data';
 import { Group } from 'src/database/entities/group.entity';
 import { TagGroup } from 'src/database/entities/tag-group.entity';
 import { Tag } from 'src/database/entities/tag.entity';
 import { UserGroup } from 'src/database/entities/user-group.entity';
 import { GroupController } from './group.controller';
 import { GroupService } from './group.service';
+import { groupImageFactory } from './utills/group.img.multer';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Group, Tag, TagGroup, UserGroup])],
+  imports: [
+    NestjsFormDataModule,
+    MulterModule.registerAsync({ useFactory: groupImageFactory }),
+    TypeOrmModule.forFeature([Group, Tag, TagGroup, UserGroup]),
+  ],
   controllers: [GroupController],
   providers: [GroupService],
 })
--- src/group/utills/group.img.multer.ts
+++ src/group/utills/group.img.multer.ts
@@ -17,22 +17,29 @@ const mkdir = (directory: string) => {
   }
 };
 
-mkdir('groupImgs');
+mkdir('groupImage');
+mkdir('backgroundImage');
 
-export const multerOptionsFactory = (): MulterOptions => {
+export const groupImageFactory = (): MulterOptions => {
   return {
     storage: multer.diskStorage({
       destination(req, file, done) { // 파일을 저장할 위치를 설정합니다
-        done(null, path.join(process.cwd()+'/public/', 'groupImgs'));
+        if(file.fieldname === 'groupImage') {
+          done(null, path.join(process.cwd()+'/public/', 'groupImage'));
+        } else {
+          done(null, path.join(process.cwd()+'/public/', 'backgroundImage'));
+        }
       },
 
       filename(req, file, done) { // 파일의 이름을 설정합니다.
+        console.log(file)
         const ext = path.extname(file.originalname); // 파일 확장자 추출
-        const basename = path.basename(file.originalname, ext); // 파일 이름
+        const basename = Math.floor(Math.random() * 100000000)
         // 파일 이름이 중복되는 것을 막기 위해 '파일이름_날짜.확장자' 의 형식으로 파일이름을 지정합니다.
         done(null, `${basename}_${Date.now()}${ext}`);  
       },
     }),
-    limits: { fileSize: 10 * 1024 * 1024 }, // 10MB로 크기를 제한
+    limits: { fileSize: 5 * 1024 * 1024 }, // 10MB로 크기를 제한
+    // fields : [{name : 'groupImage', maxCount : 1}]
   };
 };
