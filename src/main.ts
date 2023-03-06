3b94dd41f99244140eac17ec1489fe3e2276fda5
OHGILHWAN <dhrlfghks01@gmail.com>
2023-03-06 18:05:24 +0900

    Design : 정적 파일 경로 지정 main.ts 수정

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import cookieParser from 'cookie-parser';
-// import * as hbs from 'hbs'
+import * as hbs from 'hbs'
 import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true })); // transform
   app.use(cookieParser());
-  // hbs.registerPartials(join(__dirname,'..', 'views/partials'));
+  hbs.registerPartials(join(__dirname,'..', 'views/partials'));
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
