2ddb0b7a2591c9e1c66ff00741470b65aeda5d6a
OHGILHWAN <dhrlfghks01@gmail.com>
2023-03-06 20:16:11 +0900

    Design : hbs 파일 추가

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,26 @@
 import { Controller, Get } from '@nestjs/common';
+import { Render, UseGuards } from '@nestjs/common/decorators';
 import { AppService } from './app.service';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
+import { GetUser } from './common/decorator/get-user.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+  @Get('')
+  @Render('newsfeed')
+  newsfeed() {}
+
+  @Get('/groups')
+  @Render('groups')
+  group() {}
+
+  @Get('/events')
+  @Render('events')
+  event() {}
+
+  @Get('/auth')
+  @Render('auth')
+  auth() {}
 }
