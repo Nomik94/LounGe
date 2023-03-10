687afa82ccc699739613f20c6549b7854a31d4e9
OHGILHWAN <dhrlfghks01@gmail.com>
2023-03-10 18:52:20 +0900

    Feat : 그룹 멤버 리스트 axios 연결

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -28,16 +28,36 @@ export class AppController {
 
   @Get('/group/management/members')
   @Render('hub-group-members')
-  groupMembersManagement(){}
+  groupMembersManagement() {}
 
   @Get('/joined/grouplist')
   @Render('hub-group-joinedlist')
-  groupJoinedList(){}
+  groupJoinedList() {}
 
-  @Get('/group/timeline/')
+  @Get('/group/timeline')
   @Render('group-timeline')
-  async groupTimeline(@Query('groupId') groupId : number){ 
-    const a = await this.appService.groupInfo(groupId)
-    return a
+  async groupTimeline(@Query('groupId') groupId: number) {
+    if (!groupId) {
+      return;
+    }
+    return await this.appService.groupInfo(groupId);
+  }
+
+  @Get('/group/events')
+  @Render('group-events')
+  async groupEvents(@Query('groupId') groupId: number) {
+    if (!groupId) {
+      return;
+    }
+    return await this.appService.groupInfo(groupId);
+  }
+
+  @Get('/group/members')
+  @Render('group-members')
+  async groupMembers(@Query('groupId') groupId: number) {
+    if (!groupId) {
+      return;
+    }
+    return await this.appService.groupInfo(groupId);
   }
 }
