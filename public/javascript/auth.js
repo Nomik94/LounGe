074850b3c43fc043c000ad3512045d82fc7aecd0
Nomik94 <kill0374@naver.com>
2023-03-14 14:27:47 +0900

    Refactor : Auth 프론트 수정

--- public/javascript/auth.js
+++ public/javascript/auth.js
@@ -21,7 +21,7 @@ function getCookie(name) {
 
 function accessTokenExpires() {
   const accessDate = new Date();
-  accessDate.setTime(accessDate.getTime() + 1000 * 60 *60);
+  accessDate.setTime(accessDate.getTime() + 1000 * 20);
   const accessExpires = accessDate.toGMTString();
   return accessExpires;
 }
