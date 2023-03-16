4be524b2bd273f87aa84e5b1c68d1be76246db27
Nomik94 <kill0374@naver.com>
2023-03-16 23:36:26 +0900

    Refactor : 카카오로그인 수정

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -43,10 +43,8 @@ export class UserService {
   // 이메일로 유저 조회
   async getByEmail(email: string): Promise<User> {
     const user = await this.userRepository.findOne({ where: { email } });
-    if (!user) {
-      throw new NotFoundException('유저가 존재하지 않습니다.');
-    }
-    return user;
+
+    return user ?? null;
   }
 
   // 유저아이디로 유저 조회
@@ -88,7 +86,7 @@ export class UserService {
     const userId = user.id;
     await this.getById(userId);
 
-    const filename = file.filename;
+    const filename = file.key;
     await this.userRepository.update(userId, { image: filename });
   }
 
