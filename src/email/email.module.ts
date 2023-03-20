ea0ac7dc6fc2919b0188b732373c0994e3a0dcfd
Nomik94 <kill0374@naver.com>
2023-03-20 14:52:53 +0900

    Refactor : 유저가 있을 시 회원가입 이메일 인증중복체크

--- src/email/email.module.ts
+++ src/email/email.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { EmailService } from './email.service';
 import { EmailController } from './email.controller';
+import { UserService } from 'src/user/user.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/database/entities/user.entity';
 
 @Module({
-  providers: [EmailService],
+  imports: [TypeOrmModule.forFeature([User])],
+  providers: [EmailService, UserService],
   exports: [EmailService],
   controllers: [EmailController],
 })
