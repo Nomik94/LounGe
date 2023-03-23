fd3f8886e4447aebfa8281f69b6abf3b5a392975
Nomik94 <kill0374@naver.com>
2023-03-23 16:42:07 +0900

    Refactor : 댓글 그룹가입자만 생성할수 있게 수정

--- src/common/repository/user.repository.ts
+++ src/common/repository/user.repository.ts
@@ -7,4 +7,12 @@ export class UserRepository extends Repository<User> {
   constructor(private dataSource: DataSource) {
     super(User, dataSource.createEntityManager());
   }
+  async checkGroupByUserId(userId: number) {
+    return await this.createQueryBuilder('user')
+      .select(['group.id', 'user.id', 'userGroups.group.id'])
+      .leftJoin('user.userGroups', 'userGroups')
+      .leftJoin('userGroups.group', 'group')
+      .where('user.id=:id', { id: userId })
+      .getOne();
+  }
 }
