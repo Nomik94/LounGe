3748075ae51a7bf7a5dc23877c552b1f3d0fb196
OHGILHWAN <dhrlfghks01@gmail.com>
2023-04-06 01:44:46 +0900

    Test : 테스트 코드 작성 파일 추가

---
+++ src/calendar/test/calendar.controller.spec.ts
@@ -0,0 +1,20 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { CalendarController } from '../calendar.controller';
+
+
+
+describe.skip('CalendarController', () => {
+  let controller: CalendarController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [CalendarController],
+    }).compile();
+
+    controller = module.get<CalendarController>(CalendarController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
